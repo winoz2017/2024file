80c5c084d140d7c5eaa16f70f7e81a1d198f6a39
winoz <winoz@naver.com>
2024-01-08 23:01:52 +0900

    ddd

---
+++ src/components/ImageList.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react';
+
+function ImageList({ imgData }) {
+  return (
+    <>
+      <div style={{ display: 'flex', flexWrap: 'wrap', gap: 10 }}>
+        {imgData.length > 0 ? (
+          imgData.map((item, i) => {
+            return (
+              <div key="i">
+                <img src={`http://localhost:8080/view/file/${item.id}`} alt="" style={{ width: 200 }} />
+              </div>
+            );
+          })
+        ) : (
+          <p>no image</p>
+        )}
+      </div>
+    </>
+  );
+}
+export default ImageList;
